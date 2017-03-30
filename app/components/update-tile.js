7747536566ee79beebf5c7edde91ef62cecf24be
Yonkokilasi <yonkokilasi@gmail.com>
2017-03-30 16:16:14 +0300

    Save Button now working A user is now able to update a blog post

--- app/components/update-tile.js
+++ app/components/update-tile.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePost(){
+      this.set('updatePostForm',true);
+    },
+    update(post) {
+      var params = {
+        owner: this.get("owner"),
+        head: this.get('head'),
+        body: this.get('body'),
+        snippet: this.get('snippet'),
+        image: this.get('image'),
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+  }
+},
 });
